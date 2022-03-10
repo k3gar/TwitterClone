51b2e96be1b632ee9d983b5c10505d963685ec90
Kevin Garcia <kevinede40@gmail.com>
2022-03-10 15:39:24 -0600

    Login Form Style

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -4,16 +4,38 @@ import ('../styles/loginPage.css')
 
 
 const LoginPage = () => {
+  const [value, setValue] = React.useState(false);
+  
+  const adding = () =>{
+    const Label = document.querySelector('#text_label');
+    Label.classList.add('move')
+    Label.classList.add('change-input-login')
+  }
+  const removing = () =>{
+    const Label = document.querySelector('#text_label');
+    Label.classList.remove('change-input-login')
+    let inputArea = document.querySelector('#text').value;
+
+
+    if(inputArea != 0){
+      Label.classList.add('change-input-login-text')
+    }else{
+      Label.classList.remove('change-input-login-text')
+      Label.classList.add('move')
+
+    }
+  }
+
   return (
     <main className='loginPage'>
-        <section className='loginPage--form'>
+        <section className='loginPage--form' onMouseDown={() => {removing();}}>
             <div>
             <h3>Sign in to Twitter</h3>
                 <Buttons/>
                 {/* <span className="or-divider">or</span> */}
-                <label className='loginPage--form__input change-input-login' htmlFor="text">
+                <label id='text_label' className='loginPage--form__input' htmlFor="text" onMouseUp={()=> {adding();}}>
                   Phone, email, or username
-                  <input id='text' onClick={() => {console.log('click')}} className='loginPage--form__area' type="text" />
+                  <input id='text' className='loginPage--form__area' type="text" />
                 </label>
             </div>
         </section>
