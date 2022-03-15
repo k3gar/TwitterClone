2ffb8985b1a5d41be4b2521aef18f45677c5d7bb
Kevin Garcia <kevinede40@gmail.com>
2022-03-15 15:42:13 -0600

    Header Design

--- src/components/TweetInput.jsx
+++ src/components/TweetInput.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 import '../styles/tweetInput.css'
 
 const TweetInput = () => {
+    function prueba5() {
+        const textarea = document.querySelector("textarea");
+        textarea.addEventListener("keyup", e =>{
+          textarea.style.height = "63px";
+          let scHeight = e.target.scrollHeight;
+          textarea.style.height = `${scHeight}px`;
+        });
+    }
+
+
+
   return (
     <div className='tweetinput'>
         <div className='tweetinput--container'>
             <img className='tweetinput--container__pic' src="https://pbs.twimg.com/profile_images/1235351751802400774/LNPwr6Hv_normal.jpg" alt="profile pic" />
             <div className='tweetinput--interactive'>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <label htmlFor="post-input">
+                    <textarea onKeyUp={ () => {prueba5()}} id='post-input' type='text' className='tweetinput--interactive__input' placeholder='Whats Happening'></textarea>
+
+                </label>
                 <div className='tweetinput--interactive__assets'>
                     <div className='tweetinput--interactive__icons'>
 
@@ -23,7 +37,7 @@ const TweetInput = () => {
 
                         <svg viewBox="0 0 24 24" aria-hidden="true" class="r-icons-input "><g><path d="M12 14.315c-2.088 0-3.787-1.698-3.787-3.786S9.913 6.74 12 6.74s3.787 1.7 3.787 3.787-1.7 3.785-3.787 3.785zm0-6.073c-1.26 0-2.287 1.026-2.287 2.287S10.74 12.814 12 12.814s2.287-1.025 2.287-2.286S13.26 8.24 12 8.24z"></path><path d="M20.692 10.69C20.692 5.9 16.792 2 12 2s-8.692 3.9-8.692 8.69c0 1.902.603 3.708 1.743 5.223l.003-.002.007.015c1.628 2.07 6.278 5.757 6.475 5.912.138.11.302.163.465.163.163 0 .327-.053.465-.162.197-.155 4.847-3.84 6.475-5.912l.007-.014.002.002c1.14-1.516 1.742-3.32 1.742-5.223zM12 20.29c-1.224-.99-4.52-3.715-5.756-5.285-.94-1.25-1.436-2.742-1.436-4.312C4.808 6.727 8.035 3.5 12 3.5s7.192 3.226 7.192 7.19c0 1.57-.497 3.062-1.436 4.313-1.236 1.57-4.532 4.294-5.756 5.285z"></path></g></svg>
                     </div>
-                <button>Tweet</button>
+                <button disabled className='tweetinput--interactive__button'>Tweet</button>
                 </div>
             </div>
         </div>
